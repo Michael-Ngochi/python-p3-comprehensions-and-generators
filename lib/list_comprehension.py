2eabd5188cc6f6d1d811280dfc5a4ee3083867b8
Michael-Ngochi <ngoochimike@gmail.com>
2025-05-12 01:03:40 +0300

    Added comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    print(num_list)
+    evens=[num for num in num_list if num%2 == 0]
+    print(evens)
+    return evens
 
 def make_exclamation(sentence_list):
+    print(sentence_list)
+    print( [f"{sentence}!" for sentence in sentence_list])
+
+    return [f"{sentence}!" for sentence in sentence_list]
     pass
+
+return_evens([5,3,4,6,8,7,5,12,10,59,32])
+make_exclamation(["Hello", "Hey", "Whats up"])
